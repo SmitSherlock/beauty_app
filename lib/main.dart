abeb11937942c0c9e34e13cd5e0b79640ece3ca9
Smit <smittalashilkar@gmail.com>
2019-07-28 13:04:56 +0530

    Symptoms and causes info added

--- lib/main.dart
+++ lib/main.dart
@@ -21,12 +21,51 @@ final fatigue = {
   ]
 };
 
+
+final fatigueInfo='Fatigue \n' 
+'Oversleeping, extreme fatigue, or staying up a few hours past your normal bedtime can cause dark circles to form under your eyes. Sleep deprivation can cause your skin to become dull and pale, allowing the dark tissues and blood vessels beneath your skin to show.\n'
+
+'Suggestion: Try to improve your sleeping pattern to reduce the dark circles.\n'
+
+'Yes, it can be cured with home remedies.\n\n';
+
+final eyeStrainInfo ='EyeStrain:\n''Using excessive mobile phones, laptops, watching television excessively can cause strain to the eyes.\n'
+'This strain can cause blood vessels around your eyes to enlarge. As a result, the skin surrounding your eyes can darken.\n'
+
+'Suggestion: Try to limit the usage of things that cause strain to the eyes.\n'
+
+'Yes, it can be treated with home remedies.\n\n';
+
+final allergiesInfo='Allergy:\n''Allergic reactions can cause your blood vessels to dilate and become more visible beneath your skin.\n'
+'This causes dark circles.\n'
+'It can be treated with home remedies.\n\n';
+
+
+final dehydrationInfo='Dehydration:\n''Dehydration causes dark circles around the eyes.\n'
+'When your body is not receiving the proper amount of water, the skin beneath your eyes begins to look dull and your eyes look sunken.\n'
+
+'Suggestion: Try to take enough amount of water and juices to keep the skin hydrated.\n'
+
+'Yes, it can be treated with home remedies.\n';
+
+final sunExposure='Sun Exposure:\n''Sun exposure causes melanin production in the skin which gives pigment to the skin.\n'
+'Excessive exposure to the sun can cause dark circles as the skin under the eyes is more sensitive.\n'
+'Suggestion: Try to cover your face when you move out in the sun and apply sunscreen whenever moving out in the sun.\n'
+'Yes, it can be treated with home remedies.\n';
+
+final genetics='Genetics:\n''Dark circles can be an inherited trait seen early in childhood and may worsen as you age or slowly disappear.\n'
+'No, it cannot be treated with home remedies but it can be reduced to some extent.\n';
+
+
 // 'Question':'How is your sleep patten',: ['Oversleep (10-12 hrs)', 'No'],
 // 'Do you have a frequent headache?': ['Yes', 'No'],
 // 'Are you facing any kind of stress?': ['Yes', 'No'],
 // 'How are your eyes?': ['Watery','Dry','Normal'],
 // ' Do you have blurred vision at times?':['Yes','No'],
 // 'Do you blink a lot while using a mobile or watching tv?':['Yes','No']
+
+var causesList=[''];
+
 
 var fatigueScore = 0;
 var eyeStrainScore = 0;
@@ -774,17 +813,29 @@ class Answer extends StatelessWidget {
   const Answer({Key key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
+    final darkcircleInfo='Dark circles:\n'
+                  'These are dark colored blemishes around the eyes.\n'
+                  'They may be due to different underlying causes.\n'
+                  'The conditions will differ from person to person.\n\n';
+    var finalString='';
     if (fatigueScore >= 2) {
+
+      finalString+=darkcircleInfo+fatigueInfo+eyeStrainInfo+allergiesInfo+dehydrationInfo+genetics;
+    }
       return Scaffold(
-          body: Center(
-        child: Text(
-          'Dark circles:\n'
-          'These are dark colored blemishes around the eyes.\n'
-          'They may be due to different underlying causes.\n'
-          'The conditions will differ from person to person.\n',
-          textDirection: TextDirection.ltr,
+        body: ListView(
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.all(16.0),
+              child: Text(
+                finalString,
+                textDirection: TextDirection.ltr,
+                style: TextStyle(fontWeight: FontWeight.w500, fontSize: 15,height: 1.5,decorationStyle: TextDecorationStyle.wavy,
+)),
+            )
+          ],
         ),
-      ));
+      );
     }
     // else if()
     // {
@@ -793,7 +844,7 @@ class Answer extends StatelessWidget {
 
     // appBar: AppBar(
   }
-}
+
 
 class QuestionBuilder extends StatelessWidget {
   const QuestionBuilder({Key key}) : super(key: key);
